6acfa8673f7f456df81e341d3722b2d4caf68371
sanserif28 <sanserif28@gmail.com>
2025-05-02 23:25:25 +0900

    init project

---
+++ src/main/java/com/apiece/springboottwitter/SpringboottwitterApplication.java
@@ -0,0 +1,13 @@
+package com.apiece.springboottwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottwitterApplication.class, args);
+	}
+
+}
